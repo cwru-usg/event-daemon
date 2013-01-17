967f70ca5eb67a3685573c261248b5897e591b80
Tom Dooner <tom@causes.com>
2013-01-17 10:41:23 -0500

    Empty Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UsgEventDaemon::Application.config.secret_token = '02a717ec5c526274a42fe3317a3d915770f099b001d30d5dcf5a716fcb2e419412e4a5d407efef3643968e568e26f1771d6cbba03a039b475cfca4c47e7fdccb'
