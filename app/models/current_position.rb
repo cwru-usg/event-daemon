a685679938a47f8c4e831554448a2f65d2c6d49a
Tom Dooner <tom@causes.com>
2013-01-28 16:57:36 -0500

    Create validations on CurrentPosition model

--- app/models/current_position.rb
+++ app/models/current_position.rb
@@ -1,6 +1,8 @@
 class CurrentPosition < ActiveRecord::Base
   belongs_to :organization
   belongs_to :user
+  validates_presence_of :user_id
+  validates_presence_of :organization_id
 
   attr_accessible :name, :user
 end
