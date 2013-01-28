d29ae21f72f682764d32ffced63a4c697ae2e4a0
Tom Dooner <tom@causes.com>
2013-01-28 01:35:18 -0500

    Prepare for deployment

--- Gemfile
+++ Gemfile
@@ -17,7 +17,7 @@ group :assets do
   gem 'coffee-rails', '~> 3.2.1'
 
   # See https://github.com/sstephenson/execjs#readme for more supported runtimes
-  # gem 'therubyracer', :platforms => :ruby
+  gem 'therubyracer', :platforms => :ruby
 
   gem 'uglifier', '>= 1.0.3'
 end
@@ -28,6 +28,7 @@ group :development, :test do
   gem 'rspec-rails'
   gem 'factory_girl_rails', '~> 4.0'
   gem 'mocha', '0.12.7'
+  gem 'capistrano'
 end
 
 
---
+++ config/deploy.rb
@@ -0,0 +1,41 @@
+require 'bundler/capistrano'
+
+default_run_options[:pty] = true
+ssh_options[:forward_agent] = true
+ssh_options[:keys] = %w(~/.ssh/id_rsa.pub)
+
+set :application, "event-daemon"
+set :domain,      "usg.case.edu"
+set :repository,  "git://github.com/cwru-usg/event-daemon.git"
+set :use_sudo,    false
+set :user,        "usgit-deploy"
+set :deploy_to,   "/var/deploy/#{application}"
+set :scm,         "git"
+
+role :app, domain
+role :web, domain
+role :db,  domain, :primary => true
+
+namespace :deploy do
+  task :start, :roles => :app do
+    run "touch #{current_release}/tmp/restart.txt"
+  end
+
+  task :stop, :roles => :app do
+    # Do nothing.
+  end
+
+  desc "Restart Application"
+  task :restart, :roles => :app do
+    run "touch #{current_release}/tmp/restart.txt"
+  end
+
+  namespace :assets do
+    task :precompile, :roles => :app do
+      run 'rake assets:precompile'
+    end
+  end
+end
+
+after 'deploy', 'deploy:migrate'
+after 'deploy', 'deploy:assets:precompile'
