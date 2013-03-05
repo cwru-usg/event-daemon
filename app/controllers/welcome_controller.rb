ab40a0fbb89b5351b04f95a4f851897a73e8b7de
Tom Dooner <tom@causes.com>
2013-03-05 18:02:58 -0500

    Give some fun statistics on the front page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,11 @@
 class WelcomeController < ApplicationController
   def index
-    @events = Event.where(['starts < ? AND starts > ?', Time.now + 7.days, Time.now]).includes(:organization)
+    @statistics = {
+      :num_events => Event.count,
+      :num_organizations => Organization.count,
+      :total_reclaimed => Event.sum(:reclaimed_amount),
+      :total_reclaimed_canceled_events => Event.where('reclaimed_amount IS NOT NULL AND canceled = ?', true).count,
+      :total_reclaimed_events => Event.where('reclaimed_amount IS NOT NULL AND canceled = ?', false).count,
+    }
   end
 end
