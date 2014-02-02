4f780f8d3086a62863fd5bf7e50d8b376eaa88ef
Tom Dooner <tom.dooner@causes.com>
2014-02-02 14:22:53 -0800

    Upgrade to CollegiateLink 0.3
    It has support for the new API

--- config/initializers/collegiatelink.rb
+++ config/initializers/collegiatelink.rb
@@ -1,9 +1,11 @@
-ENV['CL_APIKEY']     ||= 'your api key'
-ENV['CL_IP']         ||= 'your ip'
-ENV['CL_SHAREDKEY']  ||= 'your shared key'
-ENV['CL_SOCKS']      ||= 'your socks proxy'
-ENV['CL_SOCKS_PORT'] ||= 'your socks proxy port'
-COLLEGIATELINK = CollegiateLink::Client.new(ENV['CL_APIKEY'], ENV['CL_IP'], ENV['CL_SHAREDKEY'])
+ENV['CL_APIKEY']      ||= 'your api key'
+ENV['CL_IP']          ||= 'your ip' # or nil
+ENV['CL_PRIVATEKEY']  ||= 'your private key'
+ENV['CL_SOCKS']       ||= 'your socks proxy'
+ENV['CL_SOCKS_PORT']  ||= 'your socks proxy port'
+COLLEGIATELINK = CollegiateLink::Client.new(apikey: ENV['CL_APIKEY'],
+                                            ip: ENV['CL_IP'],
+                                            privatekey: ENV['CL_PRIVATEKEY'])
 COLLEGIATELINK.use_socks_proxy(ENV['CL_SOCKS'], ENV['CL_SOCKS_PORT'])
 
 USG_MEMBERS = COLLEGIATELINK.roster(14280)
